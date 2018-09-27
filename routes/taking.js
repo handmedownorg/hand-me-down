3b65689d4c638fd08a3f03303fbc36108ef691e3
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-09-27 13:23:30 +0200

    Tag comparison by photoAPI working

--- routes/taking.js
+++ routes/taking.js
@@ -11,46 +11,66 @@ const { resolveAfterWait } = require("../AzureOcrAPI/getTextFromPhoto");
 
 //this routes are requested after mail confirmation
 
-router.get("/take/:itemID", ensureLogin.ensureLoggedIn('/'), (req, res, next) => {
- 
-  const itemID = encodeURIComponent(req.params.itemID);
-  Item.findById(itemID).
-    populate("statusID")
-    .then(item => {
-      const user = req.user;
-      //console.log("item + user: " + item, user)
-      res.render("items/take", { item, user })
-    })
-    .catch(e => console.log(e))
-});
+router.get(
+  "/take/:itemID",
+  ensureLogin.ensureLoggedIn("/"),
+  (req, res, next) => {
+    const itemID = encodeURIComponent(req.params.itemID);
+    Item.findById(itemID)
+      .populate("statusID")
+      .then(item => {
+        const user = req.user;
+        //console.log("item + user: " + item, user)
+        res.render("items/take", { item, user });
+      })
+      .catch(e => console.log(e));
+  }
+);
 
-
-
-router.post("/taken/:itemID", uploadCloud.single("tag-photo"), ensureLogin.ensureLoggedIn("/"),(req, res, next) => {
+router.post(
+  "/taken/:itemID",
+  uploadCloud.single("tag-photo"),
+  ensureLogin.ensureLoggedIn("/"),
+  (req, res, next) => {
     const itemID = encodeURIComponent(req.params.itemID);
     const newKeeper = req.user;
     const imgPath = req.file.url;
-    console.log("This is the image path " + imgPath);
+    //console.log("This is the image path " + imgPath);
+    let tagFromAPI;
 
-      Item.findById(itemID)
-      .then(item => {
-        itemVar = item;
-        return Status.findById(item.statusID);
-      })
-      .then(status => {
-        console.log("The keeper was " + status.currentHolderID);
-        return Status.findByIdAndUpdate(
-          { _id: status._id },
-          { currentHolderID: newKeeper._id }
-        );
-      })
-      .then(status => {
-        console.log("Now the keeper is " + status.currentHolderID);
-        res.render("items/confirmation");
-      })
-      .catch(err => {
-        res.render("error", { message: "Keeper not found" });
-      }); 
+    getTextFromPhoto(imgPath)
+      .then(url => resolveAfterWait(5000, url))
+      .then(textTag => {
+        console.log("The TAG goes through create /POST " + textTag);
+        tagFromAPI = textTag;
+        Item.findById(itemID)
+          .then(item => {
+            if (item.tag == tagFromAPI) {
+              console.log(
+                "After verification the tags match => IT'S THE SAME BOOK"
+              );
+            } else {
+              console.log(
+                "After verification the don't tags match => IT'S NOT THE SAME BOOK"
+              );
+            }
+            return Status.findById(item.statusID);
+          })
+          .then(status => {
+            console.log("The keeper was " + status.currentHolderID);
+            return Status.findByIdAndUpdate(
+              { _id: status._id },
+              { currentHolderID: newKeeper._id }
+            );
+          })
+          .then(status => {
+            console.log("Now the keeper is " + status.currentHolderID);
+            res.render("items/confirmation");
+          })
+          .catch(err => {
+            res.render("error", { message: "Keeper not found" });
+          });
+      });
   }
 );
 
